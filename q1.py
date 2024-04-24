5ed2866a5fc24b83db5cbe177b2711af2b37266a
cam <cambo9p.11@gmail.com>
2024-04-24 17:36:19 +1200

    passed two sum

---
+++ q1.py
@@ -0,0 +1,19 @@
+def twoSum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    for i in range(len(nums)):
+        diff = target - nums[i]
+        for j in range(len(nums)):
+            if i == j:
+                continue
+            if nums[j] == diff:
+                return [i, j]
+
+    return None
+
+
+print(twoSum([2, 7, 11, 15], 9))
+print(twoSum([3, 2, 4], 6))
